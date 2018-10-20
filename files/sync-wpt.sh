29f875e5f2cebe62e1d826ad162edc20629ac253
Mike Pennisi <mike@mikepennisi.com>
2018-10-19 20:14:25 -0400

    Centralize logging
    Replace cron job with systemd timer so that all relevant events are
    logged in the same format and rotated accordingly.

--- files/sync-wpt.sh
+++ files/sync-wpt.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-cd /root/web-platform-tests/
-
 function parse_git_hash() {
   git rev-parse --short HEAD 2> /dev/null
 }
 
 GIT_HEAD=$(parse_git_hash)
 
-git pull origin master
+git pull --quiet origin master
 
 GIT_NEW_HEAD=$(parse_git_hash)
 
-
 if [ "$GIT_HEAD" != "$GIT_NEW_HEAD" ]
 then
-    echo "web-platform-tests HEAD is now ${GIT_NEW_HEAD} restarting wpt service"
+    echo "HEAD changed (${GIT_HEAD} -> ${GIT_NEW_HEAD}). Restarting service."
     systemctl restart wpt
+else
+    echo "HEAD unchanged (${GIT_HEAD}). Taking no action."
 fi
 
