083d5b8a0088d53b40c2c384a4bfbe5adc443a76
Mike Pennisi <mike@mikepennisi.com>
2019-04-11 12:56:03 -0400

    Experiment further

--- provisioning/startup.sh
+++ provisioning/startup.sh
@@ -3,39 +3,42 @@
 mkdir /var/www-data
 chown www-data:www-data /var/www-data
 
+# The gcloud CLI which is included on the target machine image is out-of-date
+# and does not properly integrate with Docker. Install the latest version.
+# Source: https://cloud.google.com/sdk/docs/quickstart-debian-ubuntu
+
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | \
+  tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+  apt-key add -
+
+# Update the package list and install the Cloud SDK
 apt-get update
-apt-get install --yes docker.io
+apt-get --yes install google-cloud-sdk docker.io
 
 # Configure Docker to fetch images from gcloud's image repository
 gcloud auth configure-docker
 
-# The following command correctly updates the `.docker/config.json` file by
-# inserts the expected authentication data. However, it is deprecated. sudo
-# gcloud docker -- pull gcr.io/wptdashboard/web-platform-tests-live
+cat > /etc/systemd/system/web-platform-tests.service << HERE
+[Unit]
+Description=Web Platform Test Service
+After=network.target auditd.service
 
-# This command does *not* update the configuration file correctly:
-gcloud auth configure-docker
-sudo docker pull gcr.io/wptdashboard/web-platform-tests-live
+[Service]
+WorkingDirectory=/var/www-data
+User=root
+ExecStart=date > /now.txt
+KillMode=control-group
+Restart=on-failure
 
-# TODO: Figure that out.
+[Install]
+WantedBy=multi-user.target
+HERE
 
-docker run \
-	--detach \
-	--volume /var/www-data:/var/www-data \
-	--publish 80:80 \
-	--publish 443:443 \
-	--name wpt-serve \
-	wpt-serve
-
-docker run \
-	--detach \
-	--volume /var/www-data:/var/www-data \
-	--name wpt-cert \
-	wpt-cert
-
-docker run \
-	--detach \
-	--volume /var/www-data:/var/www-data \
-	--volume /run/docker.sock:/run/docker.sock \
-	--name wpt-sync \
-	wpt-sync
+systemctl enable web-platform-tests.service
