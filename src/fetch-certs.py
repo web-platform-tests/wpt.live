d9c7fb8e4887b04bccb15e040c88468970b240cb
Mike Pennisi <mike@mikepennisi.com>
2019-06-21 20:59:55 -0400

    Implement certificate fetching

---
+++ src/fetch-certs.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import argparse
+import hashlib
+import logging
+import os
+import shutil
+import subprocess
+import tempfile
+import time
+
+BUFFER_SIZE = 64 * 1024 # 64 kilobytes
+
+def setup_logging():
+    logger = logging.getLogger('fetch-certs')
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+def get_hash(file_name):
+    md5 = hashlib.md5()
+
+    with open(file_name, 'rb') as handle:
+        while True:
+            data = handle.read(BUFER_SIZE)
+            if not data:
+                break
+            md5.update(data)
+
+    return md5.hexdigest()
+
+def main(bucket_name, period):
+    logger = setup_logging()
+
+    while True:
+        logger.debug('Fetching certificates.')
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            subprocess.check_call([
+                'gsutil',
+                'cp',
+                'gs://{}/fullchain.pem'.format(bucket_name),
+                'gs://{}/privkey.pem'.format(bucket_name),
+                tmp_dir
+            ])
+
+            if get_hash('{}/fullchain.pem'.format(tmp_dir)) != get_hash('/root/fullchain.pem') or (
+                get_hash('{}/privkey.pem'.format(tmp_dir)) != get_hash('/root/privkey.pem')):
+
+                loger.debug('New files received. Copying into place.')
+
+                os.remove('/root/fullchain.pem')
+                shutil.move('{}/fullchain.pem'.format(tmp_dir), '/root/fullchain.pem')
+
+                os.remove('/root/privkey.pem')
+                shutil.move('{}/privkey.pem'.format(tmp_dir), '/root/privkey.pem')
+
+                break
+
+        logger.debug('Nothing to do. Sleeping for %d seconds.', period)
+        time.sleep(period)
+
+    logger.debug('All done')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bucket', dest='bucket_name', required=True)
+    parser.add_argument('--period', required=True, type=int)
+
+    main(**vars(parser.parse_args()))
